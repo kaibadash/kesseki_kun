be04304fd88edbeab5de6aaa5415c773d3b31596
kaiba <kaibadash@gmail.com>
2017-12-02 09:07:56 +0900

    hanami new

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/kesseki_kun'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/kesseki_kun_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/kesseki_kun_development'
+    #    adapter :sql, 'mysql://localhost/kesseki_kun_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/kesseki_kun/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
+    end
+  end
+end
---
+++ lib/kesseki_kun.rb
@@ -0,0 +1,2 @@
+module KessekiKun
+end
